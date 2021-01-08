087d5b8b349c88dc6386ac84feac2490500df7e0
Faisal Eddichari <2056026+Faisal78123@users.noreply.github.com>
2021-01-08 11:52:55 +0000

    Update myvalues.sh

--- .github/workflows/myvalues.sh
+++ .github/workflows/myvalues.sh
@@ -4,3 +4,8 @@ export TF_VAR_3=$INPUT_VARIABLE3
 echo $TF_VAR_1
 echo $TF_VAR_2
 echo $TF_VAR_3
+
+
+echo "${{ github.event.inputs.variable1 }}"
+echo "${{ github.event.inputs.variable2 }}"
+echo "${{ github.event.inputs.variable3 }}"
