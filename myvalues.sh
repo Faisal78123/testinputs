b23d3c80d3260a6575761dd59fd4311547ee166d
Faisal Eddichari <2056026+Faisal78123@users.noreply.github.com>
2021-01-08 11:55:36 +0000

    Update myvalues.sh

--- myvalues.sh
+++ myvalues.sh
@@ -1,11 +1,3 @@
-export TF_VAR_1=$INPUT_VARIABLE1
-export TF_VAR_2=$INPUT_VARIABLE2
-export TF_VAR_3=$INPUT_VARIABLE3
-echo $TF_VAR_1
-echo $TF_VAR_2
-echo $TF_VAR_3
-
-
 echo "${{ github.event.inputs.variable1 }}"
 echo "${{ github.event.inputs.variable2 }}"
 echo "${{ github.event.inputs.variable3 }}"
